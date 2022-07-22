94f653653965c31029ffe8c6115ddecb82a09a42
Bill Koplitz <bkoplitz@gmail.com>
2022-07-22 02:44:37 -0400

    build ecr and ecs from cdk

--- .ci/enable-experimental.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p $HOME/.docker;
-echo $'{\n    "experimental": true\n}' | sudo tee /etc/docker/daemon.json;
-echo $'{\n    "experimental": "enabled"\n}' | sudo tee $HOME/.docker/config.json;
-sudo service docker restart;
